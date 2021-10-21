d81290fcc4bb743576a4701555cabde2af83831c
MarijaPestoric <60827834+MarijaPestoric@users.noreply.github.com>
2021-10-21 23:29:45 +0200

    Removing global variables

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let getKeyword = document.getElementsByClassName("keyword");
-let getAudio = document.getElementsByClassName("audio");
 let power = document.getElementById("power");
-let powerAudio = document.getElementById("audioPower");
-let getTextOnOrOff = document.getElementById("text");
-let heater = document.getElementById('btn-heater');
 
 power.addEventListener("change", function () {
+  let getKeyword = document.getElementsByClassName("keyword");
+  let getTextOnOrOff = document.getElementById("text");
+  let powerAudio = document.getElementById("audioPower");
   if (power.checked) {
     powerAudio.play();
     getTextOnOrOff.textContent = "On";
@@ -20,14 +18,17 @@ power.addEventListener("change", function () {
     for (let index = 0; index < getKeyword.length; index++) {
       getKeyword[index].removeEventListener("click", playAudio);
       document.removeEventListener("keypress", sound);
-  }
+    }
   }
 });
+
 function sound(event){
   playAudio(event.key);
 };
 
   function playAudio (key) {
+    let heater = document.getElementById('btn-heater');
+    let getAudio = document.getElementsByClassName("audio");
     if (this.value === "Q" || key === "q") {
       getAudio[0].play(); 
       heater.textContent = this.id;
