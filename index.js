73e84a3f292649fbb398db9b206e2efc80f4bfad
Aiša Dervanović <59751382+dervanovicaisa@users.noreply.github.com>
2021-10-23 09:35:22 +0200

    sound

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ power.addEventListener("change", function () {
     powerAudio.play();
     getTextOnOrOff.textContent = "On";
     getTextOnOrOff.style.color = "green";
-      for (let index = 0; index < getKeyword.length; index++) {
+    for (let index = 0; index < getKeyword.length; index++) {
       getKeyword[index].addEventListener("click", playAudio);
       document.addEventListener("keypress", sound);
-    }    
+    }
   } else {
     getTextOnOrOff.textContent = "Off";
     getTextOnOrOff.style.color = "red";
@@ -22,41 +22,31 @@ power.addEventListener("change", function () {
   }
 });
 
-function sound(event){
+function sound(event) {
   playAudio(event.key);
-};
-
-  function playAudio (key) {
-    let heater = document.getElementById('btn-heater');
-    let getAudio = document.getElementsByClassName("audio");
-    if (this.value === "Q" || key === "q") {
-      getAudio[0].play(); 
-      heater.textContent = this.id;
-    } else if (this.value === "W" || key === "w") {
-      getAudio[1].play();
-      heater.textContent = this.id;
-    } else if (this.value === "E" || key === "e") {
-      getAudio[2].play();
-      heater.textContent = this.id;
-    } else if (this.value === "A" || key === "a") {
-      getAudio[3].play();
-      heater.textContent = this.id;
-    } else if (this.value === "S" || key === "s") {
-      getAudio[4].play();
-      heater.textContent = this.id;
-    } else if (this.value === "D" || key === "d") {
-      getAudio[5].play();
-      heater.textContent = this.id;
-    } else if (this.value === "Z" || key === "z") {
-      getAudio[6].play();
-      heater.textContent = this.id;
-    } else if (this.value === "X" || key === "x") {
-      getAudio[7].play();
-      heater.textContent = this.id;
-    } else if (this.value === "C" || key === "c") {
-      getAudio[8].play();
-      heater.textContent = this.id;
-    }
 }
 
-
+function playAudio(key) {
+  let heater = document.getElementById("btn-heater");
+  let getAudio = document.getElementsByClassName("audio");
+  let getVolume = document.getElementById("scale").value;
+  let bank = document.getElementById("bank");
+  if (!bank.checked) {
+    for (let i = 0; i < getAudio.length; i++) {
+      if (this.value[i] === key[i]) {
+        getAudio[i].play();
+        getAudio[i].volume = getVolume;
+        heater.textContent = this.id;
+      }
+    }
+  } else if (bank.checked) {
+    for (let i = 0; i < getAudio.length; i++) {
+      if (this.value[i] === key[i]) {
+        getAudio[i].src = getAudio[getAudio.length - i].src;
+        getAudio[i].play();
+        getAudio[i].volume = getVolume;
+        heater.textContent = this.id;
+      }
+    }
+  }
+}
